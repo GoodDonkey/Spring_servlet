4c3e50a808a765ece9c4225305e93d9eff5dc577
GoodDonkey <president20500@gmail.com>
2022-01-12 19:47:32 +0900

    Servlet: POST: HTML Form and HTTP API

---
+++ src/main/java/hi/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,29 @@
+package hi.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+                                                                                          IOException {
+        ServletInputStream inputStream = request.getInputStream(); // body 내용을 byte code로 반환한다.
+
+        // byte 코드를 String으로 바꿔준다.
+        // CharSet 을 항상 지정해줘야한다.
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("OK");
+    }
+}
